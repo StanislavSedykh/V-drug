26f81b868b2b2e8ee6c06a0126d64506d34ed01b
Stanislav Sedykh <sdsedykh@gmail.com>
2023-05-20 17:48:29 +0300

    suka

--- server/src/routes/apiRouter.js
+++ server/src/routes/apiRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const apiRouter = express.Router();
 
-const { User, Game } = require('../../db/models');
+const { User } = require('../../db/models');
 
 apiRouter.get('/scores', async (req, res) => {
   const scores = await User.findOne({ where: { id: req.session.user.id }, attributes: ['score'] });
